a4a7b6b061cb7633372c72ade034d55f61675f71
Ümmühan Öksüz <40892289+ummuhan@users.noreply.github.com>
2024-01-14 23:06:02 +0300

    functions commit

---
+++ app/src/main/java/com/uoksuz/mentoringstudies/Functions.kt
@@ -0,0 +1,14 @@
+package com.uoksuz.mentoringstudies
+
+fun main() {
+
+    fun calculate(a: Int, b: Int, c: Int) :Int{
+        return a + b + c
+    }
+    println("Toplam sayı ${calculate(10,20,30)}")
+
+    var calculate1= mathCalculate()
+
+    calculate1.calculate(12,13,14)
+    calculate1.calculate(11,15,"Domates")
+}
