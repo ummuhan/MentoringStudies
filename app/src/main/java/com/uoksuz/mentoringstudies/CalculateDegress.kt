b0d3925e1b131e2d871ceb68ba64cb620345da40
Ümmühan Öksüz <40892289+ummuhan@users.noreply.github.com>
2024-01-15 01:13:00 +0300

    Question1 commit

---
+++ app/src/main/java/com/uoksuz/mentoringstudies/CalculateDegress.kt
@@ -0,0 +1,13 @@
+package com.uoksuz.mentoringstudies
+
+class CalculateDegress {
+
+    fun calculateFahrenheit(degress: Double) {
+        println("Celsius: ${degress} Fahrenheit: ${degress * 9 / 5 + 12}")
+    }
+
+    fun calculateKelvin(degress: Double) {
+        println("Celsius: ${degress} Kelvin: ${degress + 273.15}")
+    }
+
+}
---
+++ app/src/main/java/com/uoksuz/mentoringstudies/Question1.kt
@@ -0,0 +1,12 @@
+package com.uoksuz.mentoringstudies
+
+fun main(){
+    //This app change degrees
+
+    val degree1=CalculateDegress()
+    degree1.calculateKelvin(40.5)
+    degree1.calculateFahrenheit(40.5)
+
+    degree1.calculateKelvin(35.0)
+    degree1.calculateFahrenheit(35.0)
+}
