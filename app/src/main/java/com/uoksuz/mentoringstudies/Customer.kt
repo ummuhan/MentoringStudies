f96cce9b66870ba7855e964ce99deb71ab331b0d
Ümmühan Öksüz <40892289+ummuhan@users.noreply.github.com>
2024-01-14 04:09:05 +0300

    mentoring first commit

---
+++ app/src/main/java/com/uoksuz/mentoringstudies/Customer.kt
@@ -0,0 +1,25 @@
+package com.uoksuz.mentoringstudies
+
+class Customer(var name: String, var surname: String, var age: Int) {
+
+    fun getData() {
+        println("Customer name: $name")
+        println("Customer surname: $surname")
+        println("Customer name: $age")
+    }
+
+    fun isMarried(married: Boolean, surname: String) {
+        if(married)
+            changeSurname(surname)
+    }
+
+    fun changeSurname(surname: String) {
+        println("Customer new surname ${this.surname} $surname")
+        this.surname=this.surname+" "+surname
+    }
+
+    fun newAge(){
+        println("Happy birthday!! New age is ${this.age+1}")
+        age += 1
+    }
+}
---
+++ app/src/main/java/com/uoksuz/mentoringstudies/main.kt
@@ -0,0 +1,17 @@
+package com.uoksuz.mentoringstudies
+
+fun main() {
+
+    val customer1=Customer("Ümmühan","Öksüz",25)
+    customer1.getData()
+
+    customer1.isMarried(true,"Kulaksız")
+
+    customer1.getData()
+
+    customer1.newAge()
+
+    customer1.getData()
+
+    //lateinit ifadesi primitive tiplerde geçerli değildir. Sadece referans tiplerde kullanılabilmektedir.
+}
